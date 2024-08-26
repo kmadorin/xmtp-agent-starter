80291361bd060e6c8334737d9c4a00a40de51a29
Kirill Madorin <madorinkv@gmail.com>
2024-08-26 14:17:24 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { run, HandlerContext } from "@xmtp/message-kit";
+import runAgent from './agent.js';
+
+run(async (context: HandlerContext) => {
+	// Get the message and the address from the sender
+
+	const content = context.message.content.content;
+
+	if (!content) {
+		await context.reply('Please provide a message to continue.');
+		return;
+	}
+
+	const response = await runAgent(content);
+
+	// To reply, just call `reply` on the HandlerContext.
+	await context.reply(response);
+}).then((res) => {
+	console.log('####: res: ', res);
+}).catch((e) => {
+	console.error('####: e: ', e);
+});
